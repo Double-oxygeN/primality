1921d0f9f99ea15ca0f2df44b8315abb81e47e0a
Double-oxygeN <mail@double-oxygen.net>
2020-10-10 23:48:15 +0900

    optimize iterators

--- src/primality/iterators.nim
+++ src/primality/iterators.nim
@@ -17,12 +17,21 @@ import check
 iterator primes*[T: SomeInteger](x: Slice[T]): T =
   ## Iterates over each primes between ``x``.
 
-  for p in x:
-    if isPrime(p): yield p
+  if T(2) in x:
+    yield T(2)
+
+    for p in countup(T(3), x.b, T(2)):
+      if isPrime(p): yield p
+
+  else:
+    for p in countup((x.a div T(2)) * T(2) + 1, x.b, T(2)):
+      if isPrime(p): yield p
 
 
 iterator primes*[T: SomeInteger](max: T): T =
   ## Iterates over each primes up to ``max``.
 
-  for p in T(2)..max:
+  if T(2) <= max: yield T(2)
+
+  for p in countup(T(3), max, T(2)):
     if isPrime(p): yield p
